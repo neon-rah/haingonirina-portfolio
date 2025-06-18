7f0473ec257b4cf5d44263a147b4b1af4a671134
neon-rah <haingonirina301@gmail.com>
2025-06-18 08:10:17 +0300

    config next project

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    EMAIL_USER: process.env.EMAIL_USER,
-    EMAIL_PASS: process.env.EMAIL_PASS,
-    EMAIL_SENDER: process.env.EMAIL_SENDER,
-  },
+
 };
 
 export default nextConfig;
